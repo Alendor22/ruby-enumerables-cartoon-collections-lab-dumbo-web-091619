2ab3f8de9435ff7a28fecb3d188ff4e0eab934e5
Adeja Lendor <alendor6@gmail.com>
2019-09-02 21:02:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,4 @@ def long_planeteer_calls(array)
   array.any?{ |obj| obj.length > 4}
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
-  value = array.find_index{ |obj| cheese_types.include?(obj)}
-  return array[value] unless value == nil 
-end 
+
