be6316fb02caa05ed89f124a20a1ec3f343a2864
Adeja Lendor <alendor6@gmail.com>
2019-09-02 20:53:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,4 +15,4 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-value = array.find_index {|obj| obj = }
+value = array.find_index {|obj| obj == cheese_types }
